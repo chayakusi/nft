71cedf525c6962fcd692584c3cd77fcb9c6f72c7
Tanmay <tanmay.111.222.jain@gmail.com>
2022-10-31 22:01:34 -0500

    fc

---
+++ src/components/Signup.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import { Col, Form , Button ,Card, FormControl, Row } from 'react-bootstrap'
+
+export const Signup = (props) => {
+  return (
+    <>
+    <Card>
+        <Card.Body>
+            <h2 className="text-center mb-4">Sign Up</h2>
+            <Form>
+
+            <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridFirstName">
+          <Form.Label>First Name</Form.Label>
+          <Form.Control type="firstname" placeholder="Enter First Name" />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridLastName">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control type="lastname" placeholder="LastName" />
+        </Form.Group>
+      </Row>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" controlId="formGridAddress1">
+        <Form.Label>Address</Form.Label>
+        <Form.Control placeholder="1234 Main St" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formGridPhoneNo">
+        <Form.Label>Phone Number</Form.Label>
+        <Form.Control placeholder="Phone Number" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formGridCellPhoneNo">
+        <Form.Label>Cell Phone Number</Form.Label>
+        <Form.Control placeholder="Cell-Phone Number" />
+      </Form.Group>
+
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridCity">
+          <Form.Label>City</Form.Label>
+          <Form.Control />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>State</Form.Label>
+          <Form.Select defaultValue="Choose...">
+            <option>Choose...</option>
+            <option>Alabama</option><option>Alaska</option><option>Arizona</option><option>Arkansas</option><option>California</option><option>Colorado</option><option>Connecticut</option><option>Delaware</option><option>Florida</option><option>Georgia</option><option>Hawaii</option><option>Idaho</option><option>Illinois</option><option>Indiana</option><option>Iowa</option><option>Kansas</option><option>Kentucky</option><option>Louisiana</option><option>Maine</option><option>Maryland</option><option>Massachusetts</option><option>Michigan</option><option>Minnesota</option><option>Mississippi</option><option>Missouri</option><option>Montana</option><option>Nebraska</option><option>Nevada</option><option>New Hampshire</option><option>New Jersey</option><option>New Mexico</option><option>New York</option><option>North Carolina</option><option>North Dakota</option><option>Ohio</option><option>Oklahoma</option><option>Oregon</option><option>Pennsylvania</option><option>Rhode Island</option><option>South Carolina</option><option>South Dakota</option><option>Tennessee</option><option>Texas</option><option>Utah</option><option>Vermont</option><option>Virginia</option><option>Washington</option><option>West Virginia</option><option>Wisconsin</option><option>Wyoming</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridZip">
+          <Form.Label>Zip</Form.Label>
+          <Form.Control />
+        </Form.Group>
+      </Row>
+
+      <Button className='w-100' variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+        </Card.Body>
+    </Card>
+    <div className="w-100 text-center mt-2">
+        Already have an account? Log In
+    </div>
+    </>
+  )
+}
