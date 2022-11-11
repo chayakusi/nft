8e8d5c9e3505c7877bd3fb671d26e626852b8175
Tanmay <tanmay.111.222.jain@gmail.com>
2022-11-11 15:17:30 -0600

    added balance field in signup

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,91 +1,103 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 // import Slider from './Slider';
-import '../css/Welcome.css'
-import { Link } from 'react-router-dom';
+import "../css/Welcome.css";
+import { Link } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 export default function Welcome() {
+  // const [NFTList, setNFTList] = useState([]);
+  // const getHoldings = () => {
+  //     Axios.get("http://localhost:3001/nft").then((response) => {
+  //       setNFTList(response.data);
+  //     });
+  //   };
 
-    // const [NFTList, setNFTList] = useState([]);
-    // const getHoldings = () => {
-    //     Axios.get("http://localhost:3001/nft").then((response) => {
-    //       setNFTList(response.data);
-    //     });
-    //   };
+  //nft fetch in single run
+  const { currentUser } = useAuth();
+  const [NFTdata, setNFTData] = useState([]);
+  const [userLogin, setUserLogin] = useState([]);
+  const loadNFT = async () => {
+    const response = await axios.get("http://localhost:3001/nft/get");
+    setNFTData(response.data);
+  };
 
-    //nft fetch in single run
-    const [NFTdata, setNFTData] = useState([])
-    const loadNFT = async() =>{
-      const response = await axios.get("http://localhost:3001/nft/get")
-      setNFTData(response.data)
-    }
+  const loadBalance = async () => {
+    const response = await axios.get("http://localhost:3001/api/get");
+    setUserLogin(response.data);
+  };
 
-    useEffect(()=>{
-      loadNFT()
-    },[])
-    
+  useEffect(() => {
+    loadNFT();
+  }, []);
+
+  useEffect(() => {
+    loadBalance();
+  }, []);
 
   return (
     <>
-    <div className='d-flex justify-content-center align-items-center' style={{padding:"20px"}}>
-    <div className='d-flex'>
-      <h1>Welcome,&nbsp;</h1>
-      <h1> Tanmay&nbsp;</h1>
-    </div>
-    <div className= 'p-4 bg-warning rounded-circle'></div>
-    </div>
-    {/* <div className='d-flex justify-content-center align-items-center flex-column' style={{padding:"20px"}}>
-        <div>Your Current Holdings</div>
-        <button onClick={getHoldings} style={{margin:"20px"}}>Show my Holdings</button>
-        {NFTList.map((val,key) =>{
-          
-             return (
-                <>
-                <div>
-                Name: {val.name}
-                Price: {val.price_usd}
-                Price: {val.price_eth}
-                </div>
-                <div>
-                <Slider/>
-              </div>
-              </>
-             )
-        })}
-        
-    </div> */}
-    <div className='d-flex justify-content-center align-items-center flex-column'>
-      <h3>Your Holdings</h3>
-      <div style={{margin:"20px"}}>
-        <table className='styled-table'>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Price USD</th>
-            < th>Price ETH</th>
-            </tr>
-          </thead>
-          <tbody>
-            {NFTdata.map((item,index)=>{
-              return(
-                <tr>
-                    <td>{item.name}</td>
-                    <td>{item.price_usd}</td>
-                    <td>{item.price_eth}</td>  
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ padding: "20px" }}
+      >
+        <div className="d-flex">
+          <h1>Welcome,&nbsp;</h1>
+          <h1> Tanmay&nbsp;</h1>
+        </div>
+        <div className="p-4 bg-warning rounded-circle"></div>
+      </div>
+      <div
+        style={{ margin: "10px", fontSize: "30px" }}
+        className="d-flex justify-content-center align-items-center flex-row "
+      >
+        Current Balance: $
+        <div>
+          {userLogin.map((item, index) => {
+            return <>{item.bal_usd}</>;
+          })}
+        </div>
       </div>
 
-    </div>
-    <div className='d-flex justify-content-around align-items-center' style={{padding:"20px"}}>
-        <Link to ="/buynft">
-        <button className='rounded bg-success' style={{padding:"10px"}} >Buy NFT</button></Link>
-        <Link to = "/sellnft">
-        <button className='rounded bg-danger' style={{padding:"10px"}}>Sell NFT</button></Link>
-    </div>
-
+      <div className="d-flex justify-content-center align-items-center flex-column">
+        <h3>Your Holdings</h3>
+        <div style={{ margin: "20px" }}>
+          <table className="styled-table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Price USD</th>
+                <th>Price ETH</th>
+              </tr>
+            </thead>
+            <tbody>
+              {NFTdata.map((item, index) => {
+                return (
+                  <tr>
+                    <td>{item.name}</td>
+                    <td>{item.price_usd}</td>
+                    <td>{item.price_eth}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div
+        className="d-flex justify-content-around align-items-center"
+        style={{ padding: "20px" }}
+      >
+        <Link to="/buynft">
+          <button className="rounded bg-success" style={{ padding: "10px" }}>
+            Buy NFT
+          </button>
+        </Link>
+        <Link to="/sellnft">
+          <button className="rounded bg-danger" style={{ padding: "10px" }}>
+            Sell NFT
+          </button>
+        </Link>
+      </div>
     </>
-  )
+  );
 }
--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const mysql = require("mysql");
-
 var ethers = require("ethers");
 var crypto = require("crypto");
 var id = crypto.randomBytes(32).toString("hex");
@@ -21,7 +20,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/api/get", (req, res) => {
-  const sqlSelect = "SELECT* FROM login;";
+  const sqlSelect = "SELECT * FROM login where login_id=1;";
   db.query(sqlSelect, (err, result) => {
     res.send(result);
   });
@@ -39,9 +38,10 @@ app.post("/api/insert", (req, res) => {
   const state = req.body.state;
   const zip = req.body.zip;
   const type = "GOLD";
+  const balance = req.body.balance;
 
   const sqlInsert =
-    "INSERT INTO login (email,password,first_name,last_name,address,phone,cell_phone,city,state,zip,type,eth_adr) VALUES(?,?,?,?,?,?,?,?,?,?,?,?);";
+    "INSERT INTO login (email,password,first_name,last_name,address,phone,cell_phone,city,state,zip,type,eth_adr,bal_usd) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?);";
 
   db.query(
     sqlInsert,
@@ -58,6 +58,7 @@ app.post("/api/insert", (req, res) => {
       zip,
       type,
       privateKey,
+      balance,
     ],
     (err, result) => {
       console.log(err);
@@ -83,7 +84,7 @@ app.post("/nft/buy", (req, res) => {
 
 app.get("/nft/get", (req, res) => {
   db.query(
-    "SELECT N.token_id, N.name,N.price_usd,N.price_eth FROM nft_owned O,nft_list N, login L where O.lid = L.login_id and N.token_id = O.tid;",
+    "SELECT N.token_id, N.name,N.price_usd,N.price_eth FROM nft_owned O, nft_list N where O.lid=2 and O.tid=N.token_id;",
     (err, result) => {
       if (err) {
         console.log(err);
