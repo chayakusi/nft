1f005e7ab453cfa4b76d019df01a5f709ef735b3
chaya kusi <chayakusi01@gmail.com>
2022-11-12 00:22:45 -0600

    Added Buy functionality

--- server/index.js
+++ server/index.js
@@ -2,17 +2,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const mysql = require("mysql");
+const mysql = require("mysql2");
+
 var ethers = require("ethers");
 var crypto = require("crypto");
+const { resolve } = require("path");
 var id = crypto.randomBytes(32).toString("hex");
 var privateKey = "0x" + id;
 
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "tanmay",
-  database: "nft",
+  password: "Chaya@sql123",
+  database: "mydb",
 });
 
 app.use(cors());
@@ -20,7 +22,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/api/get", (req, res) => {
-  const sqlSelect = "SELECT * FROM login where login_id=1;";
+  const sqlSelect = "SELECT * FROM login where login_id=101;";
   db.query(sqlSelect, (err, result) => {
     res.send(result);
   });
@@ -66,25 +68,70 @@ app.post("/api/insert", (req, res) => {
   );
 });
 
-app.post("/nft/buy", (req, res) => {
-  const buyname = req.body.buyname;
-  const buyid = req.body.buyid;
-  const userEmail = req.body.userEmail;
+app.post("/nft/buy", async (req, res) => {
+  const nft_id = req.body.nft_id;
 
-  const sqlUpdate = "UPDATE nft_list SET is_avl = 0  WHERE (token_id = ?);";
-  db.query(sqlUpdate, [buyid], (err, result) => {
-    console.log(err);
-  });
+  // Get the balance of buyer
+  const getBal = "SELECT bal_usd from login WHERE login_id = 101;";
+  let balance;
+  balance = await new Promise((resolve,error) => {
+    db.query(getBal, (err, result) => {
+      balance = result[0].bal_usd;
+      resolve(balance);
+      });
+  })
+  
+  //Get the price of NFT
+  const getAmt = "SELECT price_usd from nft_list where (token_id = ?);";
+  let amount;
+  amount = await new Promise((resolve, error) => {
+    db.query(getAmt, [nft_id], (err, result) => {
+      amount = result[0].price_usd;
+      resolve(amount);
+    })
+  })
+ 
+  if(balance >= amount) {
+    //Update the balance of Buyer
+    const updateBal = "UPDATE login SET bal_usd = bal_usd - ? WHERE login_id = 101;";
+    db.query(updateBal, [amount], (err, result) => {
+      console.log(err);
+    })
 
-  const sqlInsert = "INSERT INTO nft_owned VALUES (?,?,?)";
-  db.query(sqlInsert, [1, buyid, buyname], (err, result) => {
-    console.log(err);
-  });
+    //Get the Seller ID
+    const sellerId = "SELECT owner_id from nft_list where (token_id = ?);";
+    let seller_id;
+    seller_id = await new Promise((resolve, error) => {
+      db.query(sellerId, [nft_id], (err, result) => {
+        seller_id = result[0].owner_id;
+        resolve(seller_id);
+      })
+    })
+    
+    //Update the balance of Seller
+    const updateSellBal = "UPDATE login SET bal_usd = bal_usd + ? WHERE login_id = ?;";
+    db.query(updateSellBal, [amount, seller_id], (err, result) => {
+      console.log(err);
+    })
+
+    //Update the owner of NFT
+    const updateOwner = "UPDATE nft_list SET owner_id = 101 WHERE (token_id = ?);";
+    db.query(updateOwner, [nft_id], (err, result) => {
+      console.log(err);
+    })
+
+    //Make the NF available to buy
+    const sqlUpdate = "UPDATE nft_list SET is_avl = 0  WHERE (token_id = ?);";
+    db.query(sqlUpdate, [nft_id], (err, result) => {
+      console.log(err);
+    });
+  }
+
 });
 
 app.get("/nft/get", (req, res) => {
   db.query(
-    "SELECT N.token_id, N.name,N.price_usd,N.price_eth FROM nft_owned O, nft_list N where O.lid=2 and O.tid=N.token_id;",
+    "SELECT token_id,name,price_usd,price_eth FROM nft_list where owner_id=101;",
     (err, result) => {
       if (err) {
         console.log(err);
@@ -116,10 +163,6 @@ app.post("/nft/sell", (req, res) => {
     console.log(err);
   });
 
-  const sqlInsert = "DELETE FROM nft_owned WHERE (tid = ?)";
-  db.query(sqlInsert, [sellid], (err, result) => {
-    console.log(err);
-  });
 });
 
 app.listen(3001, () => {
--- server/schema.js
+++ server/schema.js
@@ -13,56 +13,43 @@ con.connect(function (err) {
   var loginTable =
     "CREATE TABLE login (login_id int NOT NULL AUTO_INCREMENT,email varchar(45) NOT NULL,password varchar(45) NOT NULL,first_name varchar(45) NOT NULL,last_name varchar(45) DEFAULT NULL,address varchar(100) NOT NULL,phone varchar(45) NOT NULL,cell_phone varchar(45) DEFAULT NULL,city varchar(45) NOT NULL,state varchar(45) NOT NULL,zip int NOT NULL,type varchar(45) NOT NULL,eth_adr varchar(100) NOT NULL,bal_usd int DEFAULT NULL,bal_eth int DEFAULT NULL,PRIMARY KEY (login_id),UNIQUE KEY phone_UNIQUE (phone));";
   var nftTable =
-    "CREATE TABLE nft_list (name varchar(45) DEFAULT NULL,token_id varchar(100) NOT NULL,nft_ethadr varchar(100) NOT NULL,price_usd int DEFAULT NULL,price_eth int DEFAULT NULL,is_avl int NOT NULL,PRIMARY KEY (token_id));";
-
-  var nftOwnedTable =
-    "CREATE TABLE nft_owned (lid int NOT NULL,tid varchar(100) NOT NULL,name varchar(45) DEFAULT NULL,PRIMARY KEY (lid,tid),KEY token_idx (tid),CONSTRAINT `login` FOREIGN KEY (`lid`) REFERENCES login (login_id),CONSTRAINT token FOREIGN KEY (tid) REFERENCES nft_list (token_id));";
-
-  con.query(nftTable, function (err, result) {
-    console.log("NFT_List Table created");
-  });
+    "CREATE TABLE nft_list (name varchar(45) DEFAULT NULL,token_id varchar(100) NOT NULL, owner_id int DEFAULT 0, nft_ethadr varchar(100) NOT NULL,price_usd int DEFAULT NULL,price_eth int DEFAULT NULL,is_avl int NOT NULL,PRIMARY KEY (token_id),CONSTRAINT owner FOREIGN KEY (owner_id) REFERENCES login (login_id));";
 
   con.query(loginTable, function (err, result) {
     console.log("Login Table created");
   });
 
-  con.query(nftOwnedTable, function (err, result) {
-    console.log("NFT_Owned Table created");
-
+  con.query(nftTable, function (err, result) {
+    console.log("NFT_List Table created");
   });
 
-  var nftInsert1 = "INSERT INTO nft_list VALUES ('A',1,1,1,1,1)";
+  var loginInsert1 = "INSERT INTO login VALUES (100,'admin@email.com','admin@123','Admin','','1234 Main St','9876543210','','Dallas','Texas','75252','GOLD','xxxx',NULL,NULL);";
+  con.query(loginInsert1, function (err, result) {
+    console.log("Record inserted");
+  });
+
+  var nftInsert1 = "INSERT INTO nft_list VALUES ('A',1,100,1,1,1,1)";
   con.query(nftInsert1, function (err, result) {
     console.log("Record inserted");
   });
 
-  var nftInsert2 = "INSERT INTO nft_list VALUES ('B',2,2,2,2,1)";
+  var nftInsert2 = "INSERT INTO nft_list VALUES ('B',2,100,2,2,1,1)";
   con.query(nftInsert2, function (err, result) {
     console.log("Record inserted");
   });
 
-  var nftInsert3 = "INSERT INTO nft_list VALUES ('C',3,3,3,3,1)";
+  var nftInsert3 = "INSERT INTO nft_list VALUES ('C',3,100,3,3,1,1)";
   con.query(nftInsert3, function (err, result) {
     console.log("Record inserted");
   });
 
-  var nftInsert4 = "INSERT INTO nft_list VALUES ('D',4,4,4,4,1)";
+  var nftInsert4 = "INSERT INTO nft_list VALUES ('D',4,100,4,4,1,1)";
   con.query(nftInsert4, function (err, result) {
     console.log("Record inserted");
   });
 
-  var nftInsert5 = "INSERT INTO nft_list VALUES ('E',5,5,5,5,1)";
+  var nftInsert5 = "INSERT INTO nft_list VALUES ('E',5,100,5,5,1,1)";
   con.query(nftInsert5, function (err, result) {
-    console.log("Record inserted");
-  });
-
-  var nftOwnedInsert1 = "INSERT INTO nft_owned VALUES(2, 1, 'A')";
-  con.query(nftOwnedInsert1, function (err, result) {
-    console.log("Record inserted");
-  });
-
-  var nftOwnedInsert2 = "INSERT INTO nft_owned VALUES(3, 1, 'A')";
-  con.query(nftOwnedInsert2, function (err, result) {
     console.log("Record inserted");
   });
 });
