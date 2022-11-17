7d4b08bc6f419e3f85becf1c7880e7869ef6c1e5
chaya kusi <chayakusi01@gmail.com>
2022-11-17 14:16:29 -0600

    Got conversion rate from API

--- client/src/components/BuyNFT.js
+++ client/src/components/BuyNFT.js
@@ -5,29 +5,60 @@ import Axios from "axios";
 import { Form, Button } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 
 const initialState = {
   nft_name: "",
   nft_id: "",
+  com_type: ""
 };
 
 export default function BuyNFT() {
   const [state, setState] = useState(initialState);
-  const { nft_name, nft_id } = state;
+  const { nft_name, nft_id, com_type } = state;
+  const [userData, setUserData] = useState([]);
   const [NFTdata, setNFTData] = useState([]);
+  const [convRate, setConvRate] = useState(0);
   const { currentUser } = useAuth();
   const userEmail = currentUser.email;
   const navigate = useNavigate();
 
+  const getConvRate = () => {
+    axios.get("https://api.coinbase.com/v2/prices/BTC-USD/buy")
+    .then((res) => {
+      console.log(parseInt(res.data.data.amount, 10));
+      setConvRate(parseInt(res.data.data.amount, 10));
+    },
+    (err) => {
+      console.log(err);
+    })
+  }
+
   const loadNFT = async () => {
+    console.log("here");
     const response = await Axios.post("http://localhost:3001/nft", {
-      userEmail: userEmail,
+      login_id: userData[0].login_id,
     });
     setNFTData(response.data);
   };
 
   useEffect(() => {
-    loadNFT();
+    Axios.post("http://localhost:3001/user", {
+      userEmail: userEmail,
+    }).then((response) => {
+      setUserData(response.data);
+    }, (err) => {
+      console.log(err);
+    }).then(() => {
+      // let login_id, bal_usd, bal_eth;
+      // login_id = userData[0].login_id;
+      // bal_usd = userData[0].bal_usd;
+      // bal_eth = userData[0].bal_eth;
+      console.log(userData);
+      loadNFT();
+    })
+    getConvRate();
+    console.log(convRate);
   }, []);
 
   const handleInputChange = (e) => {
@@ -41,20 +72,27 @@ export default function BuyNFT() {
     NFTdata.forEach((item, index) => {
       if (item.name === nft_name && item.token_id === nft_id) valid = true;
     });
-    if (!nft_name || !nft_id || !valid) {
+    if (!nft_name || !nft_id || !com_type || !valid) {
       alert("Check your values");
     } else {
-      Axios.post("http://localhost:3001/nft/buy", {
+      if((com_type == "usd" && userData[0].bal_usd < 0) || (com_type == "eth" && userData[0].bal_eth < 0)) {
+        alert("Balance is insufficient to proceed");
+      } else {
+        Axios.post("http://localhost:3001/nft/buy", {
         nft_name: nft_name,
         nft_id: nft_id,
-        userEmail: userEmail,
+        com_type: com_type,
+        login_id: userData[0].login_id,
+        bal_usd: userData[0].bal_usd,
+        bal_eth: userData[0].bal_eth
       }).then(() => {
-        setState({ nft_name: "", nft_id: "" });
+        setState({ nft_name: "", nft_id: "", com_type: "" });
       });
 
       setTimeout(() => {
         navigate("/dashboard");
       }, 500);
+      }
     }
   };
 
@@ -65,7 +103,7 @@ export default function BuyNFT() {
         className="d-flex justify-content-center align-items-center"
         style={{ margin: "20px", color: "white" }}
       >
-        <h1>Are You Ready to Buy!!</h1>
+        <h1>Are You Ready to Buy??</h1>
       </div>
       <div
         style={{ color: "white", marginTop: "100px" }}
@@ -127,6 +165,29 @@ export default function BuyNFT() {
                   placeholder="Enter ID"
                 />
               </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label className="d-flex justify-content-center align-items-center">
+                How do you want to pay the transaction fee?
+              </Form.Label>
+              <Form.Check
+                name="com_type"
+                value="usd"
+                type="radio"
+                aria-label="radio 1"
+                label="USD"
+                onChange={handleInputChange}
+                checked={com_type === "usd"}
+              />
+              <Form.Check
+                name="com_type"
+                value="eth"
+                type="radio"
+                aria-label="radio 2"
+                label="Ethereum"
+                onChange={handleInputChange}
+                checked={com_type === "eth"}
+              />
+            </Form.Group>
 
               <Button className="w-100" type="submit">
                 Buy
