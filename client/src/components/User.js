5dfc7552ce8bcabeb29537a045615be0e631e5ef
Tanmay <tanmay.111.222.jain@gmail.com>
2022-11-25 23:34:51 -0600

    user page added

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar";
 import { useAuth } from "../contexts/AuthContext";
 import Axios from "axios";
+import "../css/User.css";
+import user from "../photos/user.jpg";
 export default function User() {
   const { currentUser } = useAuth();
   const [userData, setUserData] = useState([]);
@@ -18,44 +20,121 @@ export default function User() {
     loadLogin();
   });
   return (
-    <div style={{ color: "white" }}>
+    <>
       <Navbar />
-      <div
-        style={{ marginTop: "40px" }}
-        className="d-flex justify-content-center align-items-center"
-      >
-        <h1>
-          {userData.map((item, index) => {
-            return (
-              <>
-                {item.first_name}&nbsp;{item.last_name}&nbsp;
-                <sup
-                  style={{
-                    fontFamily: "fantasy",
-                    padding: "2px",
-                    color: "grey",
-                    borderStyle: "dotted",
-                    borderRadius: "10px",
-                  }}
+
+      <div className="container mt-5 ">
+        <div className="row d-flex justify-content-center">
+          <div className="col-md-7">
+            <div
+              style={{ color: "white", backgroundColor: "transparent" }}
+              className=" card p-3 py-4"
+            >
+              <div className="text-center">
+                <img
+                  src={user}
+                  alt="userimage"
+                  width="100"
+                  className="rounded-circle"
+                />
+              </div>
+
+              <div className="text-center mt-3">
+                <span className="bg-secondary p-1 px-2 rounded text-white">
+                  {userData.map((item, index) => {
+                    return <>{item.type}</>;
+                  })}
+                </span>
+                <h5 style={{ fontSize: "30px" }} className="mt-2 mb-0">
+                  {userData.map((item, index) => {
+                    return (
+                      <>
+                        {item.first_name}&nbsp;{item.last_name}&nbsp;
+                      </>
+                    );
+                  })}
+                </h5>
+                <div>
+                  <span>
+                    {userData.map((item, index) => {
+                      return <>{item.eth_adr}</>;
+                    })}
+                  </span>
+                </div>
+                <span style={{ fontWeight: "bold" }}>
+                  {userData.map((item, index) => {
+                    return <>{item.email}</>;
+                  })}
+                </span>
+
+                <div
+                  style={{ marginTop: "20px" }}
+                  className="d-flex justify-content-around align-items-center flex-row"
                 >
-                  {item.type}
-                </sup>
-              </>
-            );
-          })}
-        </h1>
-      </div>
-      <div className="d-flex justify-contents-around align-items-center">
-        <div>
-          <label>Email: &nbsp;</label>
-          <label>
-            {userData.map((item, index) => {
-              return <>{item.email}</>;
-            })}
-          </label>
+                  <div>
+                    <div>
+                      <i>Address: &nbsp;</i>
+                      <span style={{ fontWeight: "bold" }}>
+                        {userData.map((item, index) => {
+                          return <>{item.address}</>;
+                        })}
+                      </span>
+                    </div>
+                    <div className="d-flex justify-content-start">
+                      <i>City: &nbsp;</i>
+                      <span style={{ fontWeight: "bold" }}>
+                        {userData.map((item, index) => {
+                          return <>{item.city}</>;
+                        })}
+                      </span>
+                    </div>
+                    <div className="d-flex justify-content-start">
+                      <i>State: &nbsp;</i>
+                      <span style={{ fontWeight: "bold" }}>
+                        {userData.map((item, index) => {
+                          return <>{item.state}</>;
+                        })}
+                      </span>
+                    </div>
+                    <div className="d-flex justify-content-start">
+                      <i>Zip: &nbsp;</i>
+                      <span style={{ fontWeight: "bold" }}>
+                        {userData.map((item, index) => {
+                          return <>{item.zip}</>;
+                        })}
+                      </span>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="d-flex justify-content-start">
+                      <i>Phone: &nbsp;</i>
+                      <span style={{ fontWeight: "bold" }}>
+                        {userData.map((item, index) => {
+                          return <>{item.phone}</>;
+                        })}
+                      </span>
+                    </div>
+                    <div className="d-flex justify-content-end">
+                      <i>Cell Phone: &nbsp;</i>
+                      <span style={{ fontWeight: "bold" }}>
+                        {userData.map((item, index) => {
+                          return (
+                            <>
+                              {item.phone !== ""
+                                ? item.cell_phone
+                                : "xxxxxxxxxx"}
+                            </>
+                          );
+                        })}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div>s</div>
       </div>
-    </div>
+    </>
   );
 }
